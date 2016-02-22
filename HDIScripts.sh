d20f99a627dd2537aea7f4fb7c9574e12a7e6e98
Madan Kumar <madankumar.vijayakumar@sjsu.edu>
2016-02-22 13:17:08 -0800

    commit

--- HDIScripts.sh
+++ HDIScripts.sh
@@ -47,8 +47,8 @@ workernodes=$(echo $hosts | grep -Eo '\bwn([^[:space:]]*)\b')
 echo "Extracting workernodes IP addresses"
 echo "workernodes : $workernodes" > /home/devuser/logs/workernode.log
 wnArr=$(echo $workernodes | tr "\n" "\n")
-tmpRemoteFolderName = rpmtemp
-filename = informatica_10.0.0-1.deb
+#tmpRemoteFolderName = rpmtemp
+#filename = informatica_10.0.0-1.deb
 sudo apt-get install sshpass
 for workernode in $wnArr
 do
