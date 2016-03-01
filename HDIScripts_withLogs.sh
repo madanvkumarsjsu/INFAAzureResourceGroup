0961bb85ab6b13796775573c498eae3ccf27eb72
Madan Kumar <madankumar.vijayakumar@sjsu.edu>
2016-03-01 12:15:29 -0800

    Final changes
    Commit

--- HDIScripts_withLogs.sh
+++ HDIScripts_withLogs.sh
@@ -13,30 +13,30 @@ ClusterRMSaddress=$10
 ClusterRMWaddress$11
 #Check
 mkdir /home/$VMAdminUserName/logs
-echo $VMAdminUserName $HDIClusterName $HDIClusterLoginUsername $HDIClusterLoginPassword $HDIClusterSSHHostname $HDIClusterSSHUsername $HDIClusterSSHPassword 
-#whoami > /home/$VMAdminUserName/logs/user.log
-#pwd > /home/$VMAdminUserName/logs/output.log
+echo $VMAdminUserName $HDIClusterName $HDIClusterLoginUsername $HDIClusterLoginPassword $HDIClusterSSHHostname $HDIClusterSSHUsername $HDIClusterSSHPassword > /home/$VMAdminUserName/logs/input.log
+whoami > /home/$VMAdminUserName/logs/user.log
+pwd > /home/$VMAdminUserName/logs/output.log
 
 #RPM download
 mkdir /home/$VMAdminUserName/infaRPMInstall
 cd /home/$VMAdminUserName/infaRPMInstall
-wget http://ispstorenp.blob.core.windows.net/bderpm/informatica_10.0.0-1.deb
+wget http://ispstorenp.blob.core.windows.net/bderpm/informatica_10.0.0-1.deb > /home/$VMAdminUserName/logs/download.log
 
 #Ambari API calls to extract Head node and Data nodes
 echo "Getting list of hosts from ambari"
 hostsJson=$(curl -u $HDIClusterLoginUsername:$HDIClusterLoginPassword -X GET https://$HDIClusterName.azurehdinsight.net/api/v1/clusters/$HDIClusterName/hosts)
-echo $hostsJson 
+echo $hostsJson > /home/$VMAdminUserName/logs/hosts.log
 
 echo "Parsing list of hosts"
 hosts=$(echo $hostsJson | sed 's/\\\\\//\//g' | sed 's/[{}]//g' | awk -v k="text" '{n=split($0,a,","); for (i=1; i<=n; i++) print a[i]}' | sed 's/\"\:\"/\|/g' | sed 's/[\,]/ /g' | sed 's/\"//g' | grep -w 'host_name')
-echo $hosts
+echo $hosts > /home/$VMAdminUserName/logs/parsedhost.log
 
 echo "Extracting headnode0"
 headnode0=$(echo $hosts | grep -Eo '\bhn0-([^[:space:]]*)\b') 
-echo $headnode0
+echo $headnode0 > /home/$VMAdminUserName/logs/headnode.log
 echo "Extracting headnode0 IP addresses"
 headnode0ip=$(dig +short $headnode0) 
-echo "headnode0 IP: $headnode0ip"
+echo "headnode0 IP: $headnode0ip" > /home/$VMAdminUserName/logs/headnodeip.log
 
 #Add a new line to the end of hosts file
 echo "">>/etc/hosts
@@ -46,7 +46,7 @@ echo "$headnode0ip headnode0">>/etc/hosts
 echo "Extracting workernode"
 workernodes=$(echo $hosts | grep -Eo '\bwn([^[:space:]]*)\b') 
 echo "Extracting workernodes IP addresses"
-echo "workernodes : $workernodes" 
+echo "workernodes : $workernodes" > /home/$VMAdminUserName/logs/workernode.log
 wnArr=$(echo $workernodes | tr "\n" "\n")
 #tmpRemoteFolderName = rpmtemp
 #filename = informatica_10.0.0-1.deb
@@ -55,17 +55,17 @@ for workernode in $wnArr
 do
     echo "[$workernode]" 
 	workernodeip=$(dig +short $workernode)
-	echo "workernodeip $workernodeip" 
+	echo "workernodeip $workernodeip" >> /home/$VMAdminUserName/logs/batchoutput.log
 	#create temp folder
-	sudo sshpass -p $HDIClusterSSHPassword ssh -o StrictHostKeyChecking=no $HDIClusterSSHUsername@$workernodeip "sudo mkdir ~/rpmtemp" 
+	sudo sshpass -p $HDIClusterSSHPassword ssh -o StrictHostKeyChecking=no $HDIClusterSSHUsername@$workernodeip "sudo mkdir ~/rpmtemp" >> /home/$VMAdminUserName/logs/batchoutput.log
 	#Give permission to rpm folder
-	sudo sshpass -p $HDIClusterSSHPassword ssh -o StrictHostKeyChecking=no $HDIClusterSSHUsername@$workernodeip "sudo chmod 777 ~/rpmtemp"
+	sudo sshpass -p $HDIClusterSSHPassword ssh -o StrictHostKeyChecking=no $HDIClusterSSHUsername@$workernodeip "sudo chmod 777 ~/rpmtemp" >> /home/$VMAdminUserName/logs/batchoutput.log
 	#SCP infa binaries
-	sudo sshpass -p $HDIClusterSSHPassword scp informatica_10.0.0-1.deb $HDIClusterSSHUsername@$workernodeip:"~/rpmtemp/" 
+	sudo sshpass -p $HDIClusterSSHPassword scp informatica_10.0.0-1.deb $HDIClusterSSHUsername@$workernodeip:"~/rpmtemp/" >> /home/$VMAdminUserName/logs/batchoutput.log
 	#extract the binaries
-	sudo sshpass -p $HDIClusterSSHPassword ssh -o StrictHostKeyChecking=no $HDIClusterSSHUsername@$workernodeip "sudo dpkg -i ~/rpmtemp/informatica_10.0.0-1.deb"
+	sudo sshpass -p $HDIClusterSSHPassword ssh -o StrictHostKeyChecking=no $HDIClusterSSHUsername@$workernodeip "sudo dpkg -i ~/rpmtemp/informatica_10.0.0-1.deb" >> /home/$VMAdminUserName/logs/batchoutput.log
 	#Clean the temp folder
-	sudo sshpass -p $HDIClusterSSHPassword ssh -o StrictHostKeyChecking=no $HDIClusterSSHUsername@$workernodeip "sudo rm -rf ~/rpmtemp"
+	sudo sshpass -p $HDIClusterSSHPassword ssh -o StrictHostKeyChecking=no $HDIClusterSSHUsername@$workernodeip "sudo rm -rf ~/rpmtemp" >> /home/$VMAdminUserName/logs/batchoutput.log
 done
 
 cd /home/$VMAdminUserName
